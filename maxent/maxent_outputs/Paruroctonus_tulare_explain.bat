828e46e066d88c805ca45272a65869199524dbe0
Jacob A. Gorneau <57811563+jacobgorneau@users.noreply.github.com>
2023-10-09 12:27:10 -0700

    Add files via upload

---
+++ maxent/maxent_outputs/Paruroctonus_tulare_explain.bat
@@ -0,0 +1,2 @@
+java -mx16304m -cp "/G:/My Drive/Scorpion Publication/Mapping/maxent/maxent/maxent.jar;G:\My Drive\Scorpion Publication\Mapping\maxent\maxent\maxent.jar" density.Explain -l "G:\My Drive\Scorpion Publication\Description Publications\Paruroctonus tulare\Description\data_files\maxent_outputs\Paruroctonus_tulare.lambdas" -c "G:\My Drive\Scorpion Publication\Description Publications\Paruroctonus tulare\Description\data_files\maxent_outputs\Paruroctonus_tulare.asc" "G:\My Drive\Scorpion Publication\Mapping\geographic_data\maxent_rasters"
+@if errorlevel 1 pause
